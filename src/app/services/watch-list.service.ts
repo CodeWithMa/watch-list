ec1374baeed00b9c1fc13e4ce4dd99f4adcfd9a8
Ma <101021254+CodeWithMa@users.noreply.github.com>
2026-04-01 16:16:39 +0200

    Fix watch history recording wrong episode number on series completion

--- src/app/services/watch-list.service.ts
+++ src/app/services/watch-list.service.ts
@@ -103,7 +103,7 @@ export class WatchListService {
         watchHistory: [...(item.watchHistory || []), {
           date: now,
           season: progress.season,
-          episode: progress.episode + 1
+          episode: newProgress.episode
         }]
       });
     }
