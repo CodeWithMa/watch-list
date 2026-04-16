4a608b32bb6da8bc8339265e1e2b5cfe7943ac88
Ma <101021254+CodeWithMa@users.noreply.github.com>
2026-04-16 15:18:19 +0200

    Restore :host display block and min-height on app-root

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,7 +4,8 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLink, RouterLinkActive],
-  templateUrl: './app.html'
+  templateUrl: './app.html',
+  styles: [':host { display: block; min-height: 100vh; }']
 })
 export class App {
   protected readonly title = signal('watch-list');
